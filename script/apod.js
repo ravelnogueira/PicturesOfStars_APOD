8c656eb3798b7e57cb0acde9aac08646bac3f3c0
Ravel Nogueira <88169028+nogueiraravel@users.noreply.github.com>
2021-09-21 14:55:21 -0300

    Commit para entrega, estilização depois

--- script/apod.js
+++ script/apod.js
@@ -1,7 +1,7 @@
-    $("#btn").on("click", function () {
+$("#btn").on("click", function () {
     const seach = $("#dateSeach").val()
     const key = "sJT9kQAoA7iHfUiWP5Vf0tFUJ3Um3lmYSeUrkudI"
-    var url = "https://api.nasa.gov/planetary/apod?api_key="+key+"&date="+seach;
+    var url = "https://api.nasa.gov/planetary/apod?api_key=" + key + "&date=" + seach;
 
     $.ajax({
         url: url,
@@ -14,7 +14,7 @@
             $("#title").text(resposta.title)
             $("#img").attr("src", resposta.hdurl)
             $("#explanation").text(resposta.explanation)
-            
+
         }
 
     })
